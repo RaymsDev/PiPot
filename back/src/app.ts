ca374ee30bfd118f88c74d6a2f3ac5d08ed7c902
Rayms <rayms.dev@gmail.com>
2018-01-19 16:21:33 +0100

    set http server

---
+++ back/src/app.ts
@@ -0,0 +1,23 @@
+import * as express from "express";
+
+
+class App{
+  public express : express.Application;
+
+  constructor(){
+    this.express = express();
+    this.routes();
+  }
+
+  private routes():void{
+    let router = express.Router();
+    router.get('/', (req,res,next)=>{
+      res.json({
+        message: 'Hello World!'
+      });
+    });
+    this.express.use('/', router)
+  }
+}
+
+export default new App().express;
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,17 +1,34 @@
-import * as express from "express";
+import * as http from 'http';
 
-export class Server{
-  public express : express.Application;
+import App from "./app";
 
-  constructor(){
-    this.express = express();
+const port = process.env.PORT || 3000;
+App.set('port', port);
+
+const server = http.createServer(App);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error: NodeJS.ErrnoException): void {
+  if (error.syscall !== 'listen') throw error;
+  let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+  switch(error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
   }
+}
 
-  private middleware():void{
-
-  }
-
-  private routes():void{
-
-  }
+function onListening(): void {
+  let addr = server.address();
+  let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+  console.log(`Listening on ${bind}`);
 }
